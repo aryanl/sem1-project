5a8e279298d01539b6975fccbbe404e8805d8a8b
aryanl <44058705+aryanl@users.noreply.github.com>
2018-11-22 09:11:44 +0530

    uploading code for stepper motor
    Two Stepper motors are used for the CNC drawing machine. A pen moves in the center through belts connected on the motors(one belt for one motor).The code is for making a diamond

---
+++ stepper1.ino
@@ -0,0 +1,30 @@
+#include <Stepper.h>
+int i=1;
+const int stepsPerRevolution = 200;
+Stepper myStepper1(stepsPerRevolution, 2, 3, 4, 5);
+Stepper myStepper2(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  myStepper1.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(i;i<=5;i++)
+  {
+    myStepper1.step(stepsPerRevolution);
+  }
+  for(i;i<=5;i++)
+  {
+    myStepper2.step(-stepsPerRevolution);
+  }
+  for(i;i<=5;i++)
+  {
+    myStepper1.step(-stepsPerRevolution);
+  }
+  for(i;i<=5;i++)
+  {
+    myStepper2.step(stepsPerRevolution);
+  }
+}
